9ba75facbf55c600d16e762f678133b3159a9e70
Simon <szymon.stypka@o2.pl>
2022-09-28 13:58:31 +0200

    Initial commit

---
+++ src/main/java/com/simon/futbinsolutionplayers/FutbinSolutionPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.simon.futbinsolutionplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbinSolutionPlayersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutbinSolutionPlayersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simon/futbinsolutionplayers/FutbinSolutionPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.futbinsolutionplayers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbinSolutionPlayersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
